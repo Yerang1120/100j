89a624f0a70e2a90dd9e5e4623063e69af7be0e7
이예랑 <150049595+Yerang1120@users.noreply.github.com>
2025-01-20 16:59:57 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		int a =scan.nextInt();
+		if (a==1) {
+			System.out.println("1");
+			System.out.print("1");
+		}
+		else {
+			System.out.println(a);
+			System.out.print("1");
+		}
+	}
+}
